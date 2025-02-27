8699c9ef9068b29c1b5e15b549660812ecbb0b98
Pablomou6 <pabloml2005@gmail.com>
2025-02-27 17:43:00 +0100

    retoque e avance en tiempo

--- Practica1/CodigosP1/passwd_ord.sh
+++ Practica1/CodigosP1/passwd_ord.sh
@@ -12,6 +12,12 @@ lineas_despues=$(wc -l /tmp/passwd_orignal.txt)
 echo "Lineas antes de copiar: $lineas_antes"
 echo "Lineas despues de copiar: $lineas_despues"
 
+#Calculamos la diferencia de líneas, pero debemos quitar el nombre del archivo que devuelve el comando wc
+lineas_antes=$(echo $lineas_antes | cut -d ' ' -f 1)
+lineas_despues=$(echo $lineas_despues | cut -d ' ' -f 1)
+diferencia=$(($lineas_antes-$lineas_despues))
+echo "Diferencia de líneas: $diferencia"
+
 #Ordenamos el archivo original y lo comparamos con el creado en este script. El guión se pone para ocupar el segundo parámetro, esto indica que se 
 #comparará con el input del pipe
 sort /etc/passwd | diff /tmp/passwd_orignal.txt -
---
+++ Practica1/CodigosP1/tiempo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Introduce una fecha (dd/mm/aaaa):"
+read fecha
+
+#Recuperamos el día haciendo un cut en el string. El delimitador es el slash (se indica con -d) y el campo a recuperar es el primero (se indica con -f)
+dia=$(echo $fecha | cut -d '/' -f 1)
+mes=$(echo $fecha | cut -d '/' -f 2)
+anho=$(echo $fecha | cut -d '/' -f 3)
+
+#Comprobamos si la fecha es correcta: Comparamos los rangos posibles de día, mes y año.
+if [ $dia -gt 0 -a $dia -lt 32 -a $mes -gt 0 -a $mes -lt 13 -a $anho -gt 0 ]; then
+    if [ $mes -eq 2 -a $dia -eq 29 ]; then
+        #Si el mes es febrero y el día es 29, comprobamos si el año es bisiesto.
+        #Comprobamos si el año es bisiesto; si es divisible entre cuatro y no entre cien, ó es divisible entre 400, es un año bisiesto. 
+        if [ $(($anho%4)) -eq 0 -a $(($anho%100)) -ne 0 -o $(($anho%400)) -eq 0 ]; then
+            echo "La fecha $fecha es correcta"
+        else
+            echo "La fecha $fecha es incorrecta"
+            exit 1
+        fi
+    fi
+else
+    echo "La fecha $fecha es incorrecta"
+    exit 1
+fi
+
+#Obtenemos la fecha actual. Utilizamos date con el formato %D para que nos devuelva la fecha en formato mm/dd/aaaa
+fecha_actual=$(date +%D)
+dia_actual=$(echo $fecha_actual | cut -d '/' -f 2)
+mes_actual=$(echo $fecha_actual | cut -d '/' -f 1)
+anho_actual=$(echo $fecha_actual | cut -d '/' -f 3)
+#Recuperar la hora y minutos actuales
+hora_actual=$(date +%H)
+minuto_actual=$(date +%M)
+
